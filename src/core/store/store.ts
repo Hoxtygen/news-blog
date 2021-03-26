570824e9e31e3987a6b6a5074425ee74bf8ebbad
Wasiu Idowu <hoxtygen@live.com>
2021-03-26 23:29:27 +0100

    chore: setup redux
    Redux and redux-toolkit was set up to be used for state management.
    The store have been created ready to be used.
    
    [Completed #174960343]

---
+++ src/core/store/store.ts
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./reducers/rootReducer";
+import logger from "redux-logger";
+
+
+const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+    devTools: process.env.NODE_ENV !== 'production',
+})
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch;
+
+
+export default store
